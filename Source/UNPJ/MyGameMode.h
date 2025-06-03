4b8506e6ed2f732ba0de0f086cc442f109c4bb84
hy2n416 <hy2n416@gmail.com>
2025-06-03 21:22:52 +0900

    HUD update

--- Source/UNPJ/MyGameMode.h
+++ Source/UNPJ/MyGameMode.h
@@ -26,23 +26,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
 	TSubclassOf<UUserWidget> TitleScreenWidgetClass;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
-	TSubclassOf<UUserWidget> OptionsWidgetClass;
-	
 protected:
 	// 생성된 타이틀 화면 위젯 인스턴스
 	UPROPERTY()
 	UUserWidget* CurrentTitleScreenWidget;
 
-	UPROPERTY()
-	UUserWidget* CurrentOptionsWidget;
-
-public:
-	// 옵션 위젯을 띄우는 함수 (UMG에서 호출할 수 있도록 BlueprintCallable 선언)
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void ShowOptionsScreen();
-
-	// 옵션 위젯을 숨기는 함수 (UMG에서 호출할 수 있도록 BlueprintCallable 선언)
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void HideOptionsScreen();	
 };
